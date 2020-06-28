b9d08e2e3e343d78c103d7561c17b86891965175
Wangli-in <44222549+Wangli-in@users.noreply.github.com>
2020-06-28 11:41:00 +0800

    Add files via upload

---
+++ SampleCode/imobilelite-map/gen/com/supermap/imobilelite/mapsamples/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.supermap.imobilelite.mapsamples;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
